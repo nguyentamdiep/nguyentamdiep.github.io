237ae2d2fff9c90de799b21674d6ce9c2c895b1b
nguyentamdiep <tamdiepnp@gmail.com>
2026-01-03 01:05:16 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: { 'process.env': {} },
-  base: '/nguyentamdiep.github.io/'
+  base: '/'
 });
